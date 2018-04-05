04cd4c30d5949f8c88ee19d3ef2111b051c035ef
Alexandre Vieira <36221809+AlexDataBases@users.noreply.github.com>
2018-04-05 11:46:41 -0300

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+robo = ChatBot("Estudo")
+
+conversa = open("falas.txt","r")
+conversaSalva = open("falas.txt","a")
+data = conversa.read()
+robo.set_trainer(ListTrainer)
+robo.train(data)
+conversaSalva.write(data)
+while True:
+    pergunta = input()
+    resposta = robo.get_response(pergunta)
+    print(resposta)
+    conversaSalva.write(" ' "+pergunta+" ' "+",")
+    conversaSalva.write(" ' "+str(resposta)+" ' "+",")
